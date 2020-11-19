2db873ce3e412e8d1a8ce56f14f15a885375f0ba
Saila Kivelä <kivela.saila@gmail.com>
2020-11-19 09:46:14 +0200

    add JUnit tests

--- GildedRose/src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ GildedRose/src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -29,4 +29,163 @@ public class GildedRoseTest {
 		//assert quality has decreased by one
 		assertEquals("Failed quality for Dexterity Vest", 19, quality);
 	}
+	
+	@Test
+	public void testMain() {
+		
+		//launch main
+		String[] args = new String[0];
+		GildedRose.main(args);
+		
+	}
+	
+	@Test
+	public void testConsertPassQuality() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality has increased by two (perhaps should be one)
+		assertEquals("Quality consert backstage passes", 22, quality);
+	}
+	
+	@Test
+	public void testConsertPassQualityWithSellInUnder6() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality has increased by three (perhaps should be one)
+		assertEquals("Quality with sellIn under 6 consert backstage passes", 23, quality);
+	}
+	
+	@Test
+	public void testQualityWithSellInUnder0() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Conjured Mana Cake", -1, 6));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality has decreased by two
+		assertEquals("Quality with sellIn under 0 Conjured Mana Cake", 4, quality);
+	}
+	
+	@Test
+	public void testQualityWithSellInUnder0Concert() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", -1, 100));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality has decreased to 0
+		assertEquals("Quality with sellIn under 0 concert passes", 0, quality);
+	}
+	
+	@Test
+	public void testQualityWithSellInUnder0QualityUnder50Brie() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Aged Brie", -1, 40));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality has increased by 2
+		assertEquals("Quality with sellIn under 0 brie", 42, quality);
+	}
+	
+	@Test
+	public void testQualityUnder0() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Conjured Mana Cake", 1, -1));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality remains the same
+		assertEquals("Quality with quality under 0 Conjured Mana Cake", -1, quality);
+	}
+	
+	@Test
+	public void testQualityOf50() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality has increased by one
+		assertEquals("Quality with quality under 0 Concert passes", 50, quality);
+	}
+	
+	@Test
+	public void testQuality0SellInUnder0() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", -11, 0));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality remains the same
+		assertEquals("Quality with quality 0 Sulfuras", 0, quality);
+	}
+	
+	@Test
+	public void testQuality1SellInUnder0() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", -11, 1));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality remains the same
+		assertEquals("Quality with quality 0 Sulfuras", 1, quality);
+	}
+	
+	@Test
+	public void testQualityOver50SellInUnder0() {
+		//create an inn, add an item, and simulate one day
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Aged Brie", -11, 51));
+		inn.oneDay();
+		
+		//access a list of items, get the quality of the one set
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		//assert quality remains the same
+		assertEquals("Quality with quality 0 Brie", 51, quality);
+	}
 }
